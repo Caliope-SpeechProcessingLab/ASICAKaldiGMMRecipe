7ba4834627e43090c6f071a27000ee73149efe26
Andrés Lozano Durán <46720911+alozanoduran@users.noreply.github.com>
2020-01-31 13:49:48 +0100

    Add files via upload

---
+++ local/makeFeats.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#------------------------------------------------------------------SETTING GLOBAL PATHS---------------------------------------------------------------------------------------------------
+
+./path.sh
+
+#------------------------------------------------------------------- EXTRACT FEATURES ---------------------------------------------------------------------------------------------
+train_cmd="utils/run.pl"
+decode_cmd="utils/run.pl"
+
+nj=4
+# DATA PREPARATION.
+mfccdir=mfcc
+if [ ! -d $mfccdir ]
+then
+    mkdir $mfccdir
+fi
+
+x=$1
+utils/data/fix_data_dir.sh data/$x
+
+
+# MAKE MFCC PARAMETERS.
+steps/make_mfcc.sh --cmd "$train_cmd" --nj $nj data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+
+# CHECK DATA DIR
+utils/validate_data_dir.sh data/$x
---
+++ local/resetDirectory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#--------------------------------------------------------------------SETTING DIRECTORY STRUCTURE---------------------------------------------------------------------------------------------
+
+
+#Se borra la carpeta mfcc, data y exp
+rm -rf mfcc
+rm -rf exp
+rm -rf data
+
+#Se genera las carpetas vacias mfcc, data (y sus compartimentos), y exp
+cp -vr data_init data
+mkdir mfcc
+mkdir exp
+
