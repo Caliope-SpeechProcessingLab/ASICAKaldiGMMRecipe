3d6a9812e552ada72f843b6672552364f82a52f3
Andrés Lozano Durán <46720911+alozanoduran@users.noreply.github.com>
2020-07-20 12:42:29 +0200

    Add files via upload

---
+++ extra_features/make_wavelet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Lo único que se hace es llamar al archivo python para calcular los features de la energía
+
+# El archivo python para la energía está en la carpeta superior
+# cd ..
+# Se llama al script tal cual
+# echo $5
+python3 make_wavelet.py $5
+
+
+
+
