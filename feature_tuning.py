88c3a1582b1e0e496504905ba39f7ba5299c06e4
Andrés Lozano Durán <46720911+alozanoduran@users.noreply.github.com>
2020-09-15 16:09:01 +0200

    Add files via upload

--- feature_tuning.py
+++ feature_tuning.py
@@ -6,15 +6,16 @@
 
 
 import os
-import re
-import sys
 import shutil
 import subprocess
-import parselmouth
-from parselmouth.praat import call
 #import pandas as pd
 #import result_format
 import crossval_spk_functions
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
 
 # ----------------------------- FUNCTIONS  ---------------------------------- #
 def ageGroup(edad):
@@ -103,6 +104,155 @@ def create_data_path(path_name):
 #end create_data_path
 
 
+def total_accuracy(data,feature_list,values_features,path):
+    ## Aciertos en general
+    values_int = [int(x) for x in values_features]
+    spk_list = list(set(list(data["Speaker ID"])))
+
+    accuracy = list()
+    for values in values_int:
+        mean_spk = list()
+        for spk in spk_list:
+            mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["Speaker ID"] == spk), ["Correct"]])
+            if not math.isnan(mean_aux):
+                mean_spk.append(mean_aux)
+            # end if
+        # end for spk
+        accuracy.append(np.mean(mean_spk))
+    # end for values
+
+    plt.figure(num=None, figsize=(16, 12), dpi=120, facecolor='w', edgecolor='k')
+    plt.stem(values_int,accuracy)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Valor parámetro '+feature_list)
+    plt.title('Accuracy total, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+    plt.grid()
+    #plt.show()
+    plt.ylim(0,1)
+    plt.savefig(os.path.join(path,'accuracy total.png'))
+    plt.close()
+# end total_accuracy
+
+
+def sex_accuracy(data,feature_list,values_features,path,l_sex):
+    ## Aciertos en general
+    values_int = [int(x) for x in values_features]
+    spk_list = list(set(list(data["Speaker ID"])))
+
+    accuracy = list()
+    for values in values_int:
+        mean_spk = list()
+        for spk in spk_list:
+            if l_sex == 'H':
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["Sex"] == l_sex) & (data["Speaker ID"] == spk), ["Correct"]])
+            elif l_sex == 'M':
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["Sex"] == l_sex) & (data["Speaker ID"] == spk), ["Correct"]])
+            # end if
+            if not math.isnan(mean_aux):
+                mean_spk.append(mean_aux)
+            # end if
+        # end for spk
+        accuracy.append(np.mean(mean_spk))
+    # end for values
+
+    plt.figure(num=None, figsize=(16, 12), dpi=120, facecolor='w', edgecolor='k')
+    plt.stem(values_int,accuracy)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Valor parámetro '+feature_list)
+    plt.grid()
+    plt.ylim(0,1)
+    if l_sex == 'H':
+        plt.title('Accuracy hombres, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy hombres.png'))
+    elif l_sex == 'M':
+        plt.title('Accuracy mujeres, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy mujeres.png'))
+    # end if
+    #plt.show()
+    plt.close()
+# end sex_accuracy
+
+
+def age_accuracy(data,feature_list,values_features,path,l_age):
+    ## Aciertos en general
+    values_int = [int(x) for x in values_features]
+    spk_list = list(set(list(data["Speaker ID"])))
+
+    accuracy = list()
+    for values in values_int:
+        mean_spk = list()
+        for spk in spk_list:
+            if l_age == 2:
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["AgeGroup"] == l_age) & (data["Speaker ID"] == spk), ["Correct"]])
+            elif l_age == 5:
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["AgeGroup"] == l_age) & (data["Speaker ID"] == spk), ["Correct"]])
+            # end if
+            if not math.isnan(mean_aux):
+                mean_spk.append(mean_aux)
+            # end if
+        # end for spk
+        accuracy.append(np.mean(mean_spk))
+    # end for values
+
+    plt.figure(num=None, figsize=(16, 12), dpi=120, facecolor='w', edgecolor='k')
+    plt.stem(values_int,accuracy)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Valor parámetro '+feature_list)
+    plt.grid()
+    plt.ylim(0,1)
+    if l_age == 2:
+        plt.title('Accuracy edad 2, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy edad 2.png'))
+    elif l_age == 5:
+        plt.title('Accuracy edad 5, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy edad 5.png'))
+    # end if
+    #plt.show()
+    plt.close()
+# end sex_accuracy
+
+def type_accuracy(data,feature_list,values_features,path,type):
+    ## Aciertos en general
+    values_int = [int(x) for x in values_features]
+    spk_list = list(set(list(data["Speaker ID"])))
+
+    accuracy = list()
+    for values in values_int:
+        mean_spk = list()
+        for spk in spk_list:
+            if type == "CorrManner":
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["Speaker ID"] == spk), ["CorrManner"]])
+            elif type == "CorrVoice":
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["Speaker ID"] == spk), ["CorrVoice"]])
+            elif type == "CorrPlace":
+                mean_aux = np.mean(data.loc[(data["Value"] == values) & (data["Speaker ID"] == spk), ["CorrPlace"]])
+            # end if
+            if not math.isnan(mean_aux):
+                mean_spk.append(mean_aux)
+            # end if
+        # end for spk
+        accuracy.append(np.mean(mean_spk))
+    # end for values
+
+    plt.figure(num=None, figsize=(16, 12), dpi=120, facecolor='w', edgecolor='k')
+    plt.stem(values_int,accuracy)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Valor parámetro '+feature_list)
+    plt.grid()
+    plt.ylim(0,1)
+    if type == "CorrManner":
+        plt.title('Accuracy modo, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy modo.png'))
+    elif type == "CorrVoice":
+        plt.title('Accuracy sonoridad, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy sonoridad.png'))
+    elif type == "CorrPlace":
+        plt.title('Accuracy lugar, max='+str(np.round(np.max(accuracy),2))+' '+feature_list+'='+str(values_int[accuracy.index(max(accuracy))]))
+        plt.savefig(os.path.join(path,'accuracy lugar.png'))
+    # end if
+    #plt.show()
+    plt.close()
+# end sex_accuracy
 
 
 def main():
@@ -116,7 +266,7 @@ def main():
     no_kal_path = 'audio/experiment_lm/no_kal_audio'
 
     result_spk= 'results/'
-    results_raw_path = 'results/raw'
+    #results_raw_path = 'results/raw'
     result_reformat = 'results/reformat/'
 
     info_user_test_path = 'info_user/test'
@@ -124,14 +274,19 @@ def main():
 
     combined_features_path = 'combined_features';
 
-    results_feat = 'tuning'
+    #results_feat = 'tuning'
     conf_path = 'conf/'
 
     # Features and values
     feat_name = 'mfcc'
-    feature_list = 'num-ceps'
-    values_features = ['4','6','8','10','12','14','16','18','20','22','24','26']
+    feature_list = 'num-mel-bins'
+    values_features = ['14','18','22','26','30','34']
 
+    # 'num-ceps'        # ['4','6','10','12','14','16','18','20','22','24','26']
+    # frame-length      # ['10','15','20','25','30','35','40']
+    # frame-shift       # ['5','10','15','20','25','30']
+    # cepstral-lifter   # ['14','18','22','26','30']
+    # num-mel-bins      # ['14','18','22','26','30','34']
 
     # Reorder spk files
     for file in os.listdir(info_user_test_path):
@@ -216,21 +371,9 @@ def main():
         os.remove(resultFilename)
         # os.rename(resultFilename, resultFilename+'_'+feature_list+values)
 
-#        # Save raw results and csv
-#        results_decode_path = 'exp/tri1/decode/scoring_kaldi/wer_details/'
-#        crossval_spk_functions.save_raw_result(results_decode_path)
-#
-#
-#        # Take .csv result and reformat for SPSS analysis
-#        for file in os.listdir(result_spk):
-#            if file.endswith(".csv"):
-#                bashCommand = "python3 result_reformat4_old.py " + os.path.join(result_spk,file) + " " + os.path.join(result_reformat,file[:-4])+'_reformat.csv'
-#                process = subprocess.Popen(bashCommand,shell=True)
-#                output, error = process.communicate()
-#
-#        # Global results for SPSS
-#        # """ SE HA COMENTADO PARA HACER LOS CÁLCULOS POR PARTES """
-#        crossval_spk_functions.global_result_reformat(result_reformat)
+        # Save raw results and csv
+        results_decode_path = 'exp/tri1/decode/scoring_kaldi/wer_details/'
+        crossval_spk_functions.save_raw_result_tuning(results_decode_path,feature_list+'_'+values)
 
     # end for values
 
@@ -258,6 +401,42 @@ def main():
         # end if
     # end for
 
+
+
+    ## --------------------- Guarda los resultados ---------------------------
+
+    # Take .csv result and reformat for SPSS analysis
+    for file in os.listdir(result_spk):
+        if file.endswith(".csv"):
+            bashCommand = "python3 result_reformat4_tuning.py " + os.path.join(result_spk,file) + " " + os.path.join(result_reformat,file[:-4])+'_reformat.csv'
+            process = subprocess.Popen(bashCommand,shell=True)
+            output, error = process.communicate()
+
+    # Global results for SPSS
+    # """ SE HA COMENTADO PARA HACER LOS CÁLCULOS POR PARTES """
+    crossval_spk_functions.global_result_reformat_tuning(result_reformat)
+    os.rename(result_reformat+'global_result_reformat.txt', result_reformat+'global_result_'+feature_list+'.txt')
+    ## --------------------- Guarda los resultados ---------------------------
+
+
+    file = result_reformat+'global_result_'+feature_list+'.txt'
+
+    data = pd.read_csv(file, sep="\t", header='infer')
+    #data = data.iloc[1:]
+    data.columns = ["N", "Speaker ID", "Task", "Evaluation", "Hit", "Correct", "Target", "Response", "Target Utterance", "AgeGroup", "Sex", "Feature", "Value", "CorrManner", "CorrVoice", "CorrPlace", "Last"]
+    data = data.drop(['N', 'Last'], axis=1)
+
+    total_accuracy(data,feature_list,values_features,result_reformat)
+
+    sex_accuracy(data,feature_list,values_features,result_reformat,'H')
+    sex_accuracy(data,feature_list,values_features,result_reformat,'M')
+
+    age_accuracy(data,feature_list,values_features,result_reformat,2)
+    age_accuracy(data,feature_list,values_features,result_reformat,5)
+
+    type_accuracy(data,feature_list,values_features,result_reformat,"CorrManner")
+    type_accuracy(data,feature_list,values_features,result_reformat,"CorrVoice")
+    type_accuracy(data,feature_list,values_features,result_reformat,"CorrPlace")
 # end main
 
 
@@ -266,8 +445,6 @@ if __name__ == "__main__":
    # main(sys.argv)
    main()
 # end if
-
-
 
 
 
